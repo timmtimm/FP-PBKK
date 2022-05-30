ed2a5f15c503547da803b11e88248d138769d1f8
Rolanov <99122278+Rolanov@users.noreply.github.com>
2022-05-30 18:23:20 +0700

    Update create.blade.php

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -4,7 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <form action="{{ route('food.store') }}" method="post">
+            @if(Session::has('message'))
+            <div class = "alert alert-success">
+                {{ Session::get('message') }}
+            @endif
+            <form action="{{ route('food.store') }}" method="post" enctype="multipart/form-data">
             @csrf
             <div class="card">
                 <div class="card-header">Add new food</div>

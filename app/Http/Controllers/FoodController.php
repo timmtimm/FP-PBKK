7542de735212f3aa215d6bc6e49f3cba849ff691
Rolanov <99122278+Rolanov@users.noreply.github.com>
2022-05-30 19:56:45 +0700

    Update FoodController.php

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -63,8 +63,9 @@ class FoodController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
-    {
-        //
+    {        
+        $food = Food::find($id);
+        return view('food.edit', compact('food'));
     }
 
     /**
@@ -76,7 +77,31 @@ class FoodController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'name'=>'required',
+            'description' => 'required',
+            'price' => 'required|integer',
+            'category' => 'required',
+            'image' => 'required|mimes:png,jpeg,jpg'
+        ]);
+
+        $food = Food::find($id);
+        $name = $food->image;
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $name = time().'.'.$image->getClientOriginalExtension();
+            $destinationPath = public_path('/image');
+            $image->move($destinationPath, $name);
+        }
+
+        $food->name = $request->get('name');
+        $food->description = $request->get('description');
+        $food->price = $request->get('price');
+        $food->category_id = $request->get('category');
+        $food->image = $name;
+        $food->save();
+
+        return redirect()->route('food.index')->with('message', 'Food information updated');
     }
 
     /**
